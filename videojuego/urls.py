4eb187f4fb4f16ec34a570217a65f01665e9e095
InigoTec <a01023684@itesm.mx>
2021-04-14 13:47:59 -0500

    nuevos

--- videojuego/urls.py
+++ videojuego/urls.py
@@ -4,7 +4,13 @@ from . import views
 urlpatterns = [
     path('',views.index, name='index'),
     path('indicadores/', views.indicadores, name='indicadores'),
+    path('login/', views.login, name='login'),
+    path('register/', views.register, name='register'),
     
     # path('proceso', views.proceso, name = 'proceso'),
     # path('datos',views.datos, name = 'datos'),
 ]
+
+#urlpatterns += [
+ #   path('accounts/', include('django.contrib.auth.urls')),
+#]
--- videojuego/views.py
+++ videojuego/views.py
@@ -1,6 +1,10 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-
+from django.views.decorators.csrf import csrf_exempt
+from django.contrib.auth.decorators import login_required
+from json import loads
+# from . models import Reto
+import psycopg2
 # Create your views here.
 
 def index(request):
@@ -9,3 +13,9 @@ def index(request):
 
 def indicadores(request):
 	return render(request, 'indicadores.html')
+
+def login(request):
+	return render(request, 'registration/login.html')
+
+def register(request):
+	return render(request, 'registration/register.html')
