4da484b59f0cae364cde2b9b1af42e25fd6f45ae
Diego Jiménez Urgell <diegourgell@outlook.com>
2021-04-13 22:27:06 -0500

    Added models and changed static files

--- StarDreams/settings.py
+++ StarDreams/settings.py
@@ -11,11 +11,11 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 """
 
 from pathlib import Path
-import os
+import os, sys
+
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
-
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
@@ -33,6 +33,7 @@ ALLOWED_HOSTS = ['*']
 # Application definition
 
 INSTALLED_APPS = [
+    'videojuego.apps.VideojuegoConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -124,5 +125,6 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
-STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),)
-STATIC_ROOT = os.path.join(BASE_DIR,'staticfiles')
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR,'static/')
+]
--- videojuego/models.py
+++ videojuego/models.py
@@ -1,3 +1,61 @@
 from django.db import models
+from django.utils.translation import gettext_lazy as _
+import random, string
+
 
 # Create your models here.
+
+
+class Genero(models.IntegerChoices):
+    MUJER = 1, _('Mujer')
+    HOMBRE = 2, _('Hombre')
+    OTRO = 3, _('Otro')
+
+
+class Rama(models.IntegerChoices):
+    CIENCIA = 1, _('Ciencia')
+    TEC = 2, _('Tecnología')
+    ING = 3, _('Ingeniería')
+    MAT = 4, _('Matemáticas')
+
+
+class Profesor(models.Model):
+    nombre = models.CharField(max_length=50)
+    genero = models.IntegerField(choices=Genero.choices, default=Genero.OTRO)
+    edad = models.PositiveIntegerField()
+    correoElectronico = models.EmailField()
+    gradoEscolar = models.PositiveIntegerField()
+    token = models.CharField(max_length=10,
+                           blank=True,
+                           editable=False)
+
+    def save(self, *args, **kwargs):
+        t = ''.join(random.choices(string.ascii_letters + string.digits, k=10))
+        while Profesor.objects.filter(token=t).exists():
+            t = ''.join(random.choices(string.ascii_letters + string.digits, k=10))
+        self.token = t
+        super(Profesor, self).save(*args, **kwargs)
+
+
+class Jugador(models.Model):
+    nombre = models.CharField(max_length=50)
+    genero = models.IntegerField(choices=Genero.choices, default=Genero.OTRO)
+    edad = models.PositiveIntegerField()
+    gradoEscolar = models.PositiveIntegerField()
+    tiempoJuego = models.PositiveIntegerField()
+    ramaPreferida = models.IntegerField(choices=Rama.choices)
+    profesor = models.ForeignKey(Profesor, models.SET_NULL, blank=True, null=True)
+
+
+class Sesion(models.Model):
+    horaInicio = models.DateTimeField()
+    horaFin = models.DateTimeField()
+    jugador = models.ForeignKey(Jugador, models.CASCADE, blank=False, null=False)
+
+
+class Nivel(models.Model):
+    completado = models.BooleanField()
+    tiempo = models.PositiveIntegerField()
+    rama = models.IntegerField(choices=Rama.choices)
+    tiempoTerminacion = models.DateTimeField(null=True)
+    jugador = models.ForeignKey(Jugador, models.CASCADE, blank=False, null=False)
