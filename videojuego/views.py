0e1eae94aadc6c53170291de7b93ecc9fcaa5b0b
Diego Jiménez Urgell <diegourgell@outlook.com>
2021-04-25 02:12:20 -0500

    Update files

--- videojuego/views.py
+++ videojuego/views.py
@@ -1,11 +1,10 @@
 from functools import reduce
 
-from django.core.serializers import json
 from django.db.models import Count, Avg, Min, Max, Sum, F
 from django.shortcuts import render, redirect
 from django.http import HttpResponse, JsonResponse
 from .forms import *
-from django.contrib.auth import login, authenticate
+from django.contrib.auth import  authenticate
 from django.views.decorators.csrf import csrf_exempt
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth import login as auth_login
